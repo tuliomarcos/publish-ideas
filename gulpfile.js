363fa469d9294af8945a3cfc9f50a1fca40d312c
tsilva <tsilva@uplexis.com.br>
2019-10-11 00:53:14 -0300

    add html and css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,35 @@
 const gulp = require('gulp')
 const pug = require('gulp-pug')
 const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create();
+
+// start serve
+gulp.task('serve', () => {
+  browserSync.init({
+    server: {
+      baseDir: "./build/"
+    }
+  })
+})
 
 // compiler pug
 gulp.task('pug', () => {
-  return
-    gulp
-      .src('source/*.pug')
-      .pipe(gulp.dest('./build'))
+  return gulp
+  .src('./source/*.pug')
+  .pipe(pug())
+  .pipe(gulp.dest('./build'))
+  .pipe(browserSync.stream());
 })
 
 // compiler sass
-sass.compiler = require('node-sass');
- 
+sass.compiler = require('node-sass')
+
 gulp.task('sass', () => {
   return gulp.src('./source/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build'));
-});
- 
-gulp.task('sass-watch', () => {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream());
+})
